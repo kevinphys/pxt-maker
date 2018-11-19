504c1c0ab86e84f87819c413fffedf747910d092
lupyuen <luppy@appkaki.com>
2018-11-19 16:49:03 +0800

    Fixing startup

--- libs/core---stm32bluepill/platform.cpp
+++ libs/core---stm32bluepill/platform.cpp
@@ -7,6 +7,9 @@
 
 extern "C" void platform_setup(void);
 extern "C" void enable_debug(void);
+extern "C" void debug_print(const char *s);    //  TODO: Write a string to the buffered debug log.
+extern "C" void debug_println(const char *s);  //  TODO: Write a string plus newline to the buffered debug log.
+extern "C" void debug_flush(void);             //  TODO: Flush the buffer of the debug log so that buffered data will appear.
 
 namespace pxt {
 
@@ -22,8 +25,10 @@ namespace pxt {
 
     void platform_init() {
         //  TODO
-        platform_setup();
         enable_debug();
+        platform_setup();
+        debug_println("---pxt::platform_init"); debug_flush();  //// TODO
+
         initRandomSeed();
         //  setSendToUART(platformSendSerial);
     }
--- scripts/flash.sh
+++ scripts/flash.sh
@@ -1,1 +1,2 @@
+ls -l projects/blink/built/dockercodal/build/STM32_BLUE_PILL.bin
 openocd -f interface/stlink-v2.cfg -f target/stm32f1x.cfg -f scripts/flash.ocd
