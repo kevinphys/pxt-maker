9b04c58320447ffdd20d81f08824c86d8040b14d
lupyuen <luppy@appkaki.com>
2018-12-05 14:46:04 +0800

    Fixing dal

--- libs/core---stm32bluepill/platform.h
+++ libs/core---stm32bluepill/platform.h
@@ -20,7 +20,7 @@ extern "C" {
 }
 #endif
 
-#define PAGE_SIZE 1024
+#define NOTUSED_PAGE_SIZE 1024
 
 #define DEV_NUM_PINS 48
 
@@ -40,54 +40,55 @@ extern "C" {
 // The parameters below needs tuning!
 
 #ifdef JUST_FOR_DAL_D_TS_CPP_WILL_IGNORE
-#define PA_0 0x00
-#define PA_1 0x01
-#define PA_2 0x02
-#define PA_3 0x03
-#define PA_4 0x04
-#define PA_5 0x05
-#define PA_6 0x06
-#define PA_7 0x07
-#define PA_8 0x08
-#define PA_9 0x09
-#define PA_10 0x0A
-#define PA_11 0x0B
-#define PA_12 0x0C
-#define PA_13 0x0D
-#define PA_14 0x0E
-#define PA_15 0x0F
-#define PB_0 0x10
-#define PB_1 0x11
-#define PB_2 0x12
-#define PB_3 0x13
-#define PB_4 0x14
-#define PB_5 0x15
-#define PB_6 0x16
-#define PB_7 0x17
-#define PB_8 0x18
-#define PB_9 0x19
-#define PB_10 0x1A
-#define PB_11 0x1B
-#define PB_12 0x1C
-#define PB_13 0x1D
-#define PB_14 0x1E
-#define PB_15 0x1F
-#define PC_0 0x20
-#define PC_1 0x21
-#define PC_2 0x22
-#define PC_3 0x23
-#define PC_4 0x24
-#define PC_5 0x25
-#define PC_6 0x26
-#define PC_7 0x27
-#define PC_8 0x28
-#define PC_9 0x29
-#define PC_10 0x2A
-#define PC_11 0x2B
-#define PC_12 0x2C
-#define PC_13 0x2D
-#define PC_14 0x2E
-#define PC_15 0x2F
+
+#define NOTUSED_PA_0 0x00
+#define NOTUSED_PA_1 0x01
+#define NOTUSED_PA_2 0x02
+#define NOTUSED_PA_3 0x03
+#define NOTUSED_PA_4 0x04
+#define NOTUSED_PA_5 0x05
+#define NOTUSED_PA_6 0x06
+#define NOTUSED_PA_7 0x07
+#define NOTUSED_PA_8 0x08
+#define NOTUSED_PA_9 0x09
+#define NOTUSED_PA_10 0x0A
+#define NOTUSED_PA_11 0x0B
+#define NOTUSED_PA_12 0x0C
+#define NOTUSED_PA_13 0x0D
+#define NOTUSED_PA_14 0x0E
+#define NOTUSED_PA_15 0x0F
+#define NOTUSED_PB_0 0x10
+#define NOTUSED_PB_1 0x11
+#define NOTUSED_PB_2 0x12
+#define NOTUSED_PB_3 0x13
+#define NOTUSED_PB_4 0x14
+#define NOTUSED_PB_5 0x15
+#define NOTUSED_PB_6 0x16
+#define NOTUSED_PB_7 0x17
+#define NOTUSED_PB_8 0x18
+#define NOTUSED_PB_9 0x19
+#define NOTUSED_PB_10 0x1A
+#define NOTUSED_PB_11 0x1B
+#define NOTUSED_PB_12 0x1C
+#define NOTUSED_PB_13 0x1D
+#define NOTUSED_PB_14 0x1E
+#define NOTUSED_PB_15 0x1F
+#define NOTUSED_PC_0 0x20
+#define NOTUSED_PC_1 0x21
+#define NOTUSED_PC_2 0x22
+#define NOTUSED_PC_3 0x23
+#define NOTUSED_PC_4 0x24
+#define NOTUSED_PC_5 0x25
+#define NOTUSED_PC_6 0x26
+#define NOTUSED_PC_7 0x27
+#define NOTUSED_PC_8 0x28
+#define NOTUSED_PC_9 0x29
+#define NOTUSED_PC_10 0x2A
+#define NOTUSED_PC_11 0x2B
+#define NOTUSED_PC_12 0x2C
+#define NOTUSED_PC_13 0x2D
+#define NOTUSED_PC_14 0x2E
+#define NOTUSED_PC_15 0x2F
 #endif
 
 #endif
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,14 +1,22 @@
-# Rebuild stm32bluepill
+# Rebuild stm32bluepill.
 # rm -rf libs/stm32bluepill/built
 
-# Rebuild core
+# Rebuild core.
 rm -rf libs/core---stm32bluepill/built
 
-# Rebuild DAL
+# Rebuild DAL.
 rm libs/core---stm32bluepill/dal.d.ts
+mkdir -p libs/core---stm32bluepill/built/dockercodal
+ln -s \
+    $PWD/libs/stm32bluepill/built/dockercodal/* \
+    $PWD/libs/core---stm32bluepill/built/dockercodal/
+cat libs/core---stm32bluepill/platform.h \
+    libs/stm32bluepill/built/dockercodal/libraries/codal-libopencm3/stm32/cm/CmPinName.h \
+    > libs/core---stm32bluepill/built/dockercodal/pxtapp/platform.h
 
+# Build locally and serve.
 pxt serve --localbuild --noBrowser
-# --sourceMaps
 
+# --sourceMaps
 # mkdir -p libs/core---stm32bluepill/built/dockercodal/libraries/codal-core/inc
 # mkdir -p libs/core---stm32bluepill/built/dockercodal/pxtapp
