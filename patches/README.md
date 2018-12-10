42a5ffaf251fcf7a1c27e47204d380b15509e1f9
lupyuen <luppy@appkaki.com>
2018-12-10 10:29:19 +0800

    Working OK with GitHub Pages

--- patches/README.md
+++ patches/README.md
@@ -1,6 +1,8 @@
 # Patched files for STM32 Blue Pill
 
-This folder contains files that were patched in `pxt-common-packages` to make the compiled program run on STM32 Blue Pill:
+This folder contains files that were patched in `pxt` and `pxt-common-packages` to make the compiled program run on STM32 Blue Pill:
+
+`pxt/cli/nodeutil.ts`
 
 `pxt-common-packages/libs/base/advmath.cpp`
 
---
+++ scripts/package.sh
@@ -0,0 +1,22 @@
+# See https://makecode.com/cli/staticpkg
+
+# cd ../pxt; npm run build
+# cd ../pxt-maker; pxt staticpkg
+# pxt serve -pkg
+
+cd ../pxt-maker; pxt staticpkg --githubpages
+
+dump_flash() {
+    # Dump a UF2 file as text.
+    # UF2 file to be flashed.
+    uf2_file=projects/blink/built/binary.uf2
+
+    ls -l $uf2_file
+
+    # Convert UF2 to BIN file (flash.bin) for flashing.  Assume ROM starts at address 0x8000000.
+    python2 scripts/uf2conv.py $uf2_file --convert --output /tmp/flash.bin --base 0x8000000
+
+    # Dump the BIN file in hex format for checking.
+    hexdump -C /tmp/flash.bin >/tmp/flash.txt
+}
+# dump_flash
